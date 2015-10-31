b87a5b77353bf22092f0f343d9342520f544ecc0
Nathan Maas <nathansmaas@gmail.com>
2015-10-30 19:01:02 -0700

    setup instagram auth

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
+var instaApi = require('instagram-node').instagram();
+
+
 
 app.use(express.static(__dirname));
+
+instaApi.use({
+  client_id: process.env.instagram_client_id,
+  client_secret: process.env.instagram_client_secret
+});
+
+var redirect_uri = 'http://localhost:3000/handleauth';
+
 
 app.get('/',function(req,res){
 	res.render('index.html');
 });
+
+app.get('/authorize-user', function (req, res){
+  res.redirect(instaApi.get_authorization_url(redirect_uri));
+});
+
+app.get('/handleauth', function (req, res){
+  instaApi.authorize_user(req.query.code, redirect_uri, function(err, result){
+    if (err) {
+      res.send(err.body);
+    }else{
+      // res.send(result);
+      res.redirect('/#!/new-postcard');
+    }
+  })
+})
 
 app.listen(process.env.PORT || 3000)
--- js/controllers/NewcardCtrl.js
+++ js/controllers/NewcardCtrl.js
@@ -5,4 +5,10 @@ pennyPost.controller('NewcardCtrl',function($scope){
 		{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'},
 		{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'},
 		{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'}];
+  $scope.photoSet = false;
+  $scope.flippin = function(){
+    console.log('heeeeeyyyyyy');
+    $scope.photoSet = !$scope.photoSet;
+  }
 });
+
