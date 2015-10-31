1aa378781db0ccf4374e869104c530d8c48be7e1
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-10-30 17:03:07 -0700

    photo selector page in progress

--- js/controllers/NewcardCtrl.js
+++ js/controllers/NewcardCtrl.js
@@ -1,3 +1,8 @@
 pennyPost.controller('NewcardCtrl',function($scope){
-	$scope.hello = "heeeeeyyyyyy"
+	$scope.hello = "heeeeeyyyyyy";
+	$scope.images = [{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'},
+		{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'},
+		{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'},
+		{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'},
+		{url:'http://www.bioexpedition.com/wp-content/uploads/2012/04/Racoon-Cub.jpg'}];
 });
