c89071e0d8c7808ee77742efcf69c91f113335ae
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-11-04 16:48:16 -0800

    directive functional. styling in progress

--- js/controllers/NewcardCtrl.js
+++ js/controllers/NewcardCtrl.js
@@ -12,9 +12,24 @@ pennyPost.controller('NewcardCtrl',function($scope,$http){
     $scope.photoSet = !$scope.photoSet;
   };
   $scope.selectedImages = [];
+  $scope.style = '';
   $scope.addImage = function(image){
     if($scope.selectedImages.length < 4){
       $scope.selectedImages.push(image);
+      if($scope.selectedImages.length === 1){
+      	$scope.style = "#card-constructor img{width:100%;}"
+      }else if($scope.selectedImages.length === 2){
+      	$scope.style = "#card-constructor img{width:50%;height:100%;}"
+      }else if($scope.selectedImages.length === 3){
+      	$scope.style = "#card-constructor img:first-child{width:66%;float:left;}"+
+      	"#card-constructor img:nth-child(2){width:32%;float:right;}"+
+      	"#card-constructor img:nth-child(3){width:32%;float:right;margin-top:2%}";
+      }else if($scope.selectedImages.length === 4){
+		$scope.style = "#card-constructor img:first-child{width:50%;margin-top:-20%;}"+
+      	"#card-constructor img:nth-child(2){width:50%;margin-top:-20%;}"+
+      	"#card-constructor img:nth-child(3){width:50%;}"+
+      	"#card-constructor img:nth-child(4){width:50%;}";
+      };
     }else{
       alert('fuck you');
     };
